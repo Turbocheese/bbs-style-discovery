98e80c4d94d058dfb51c28b11f3b03302580c4a2
turbocheese <ryanmichael.lim@gmail.com>
2026-08-04 10:30:43 +0800

    Bump cache version for wow-factor polish

--- sw.js
+++ sw.js
@@ -6,12 +6,12 @@
 // IMPORTANT: bump CACHE_VERSION on every deploy that changes any
 // cached file (the ?v= params below must match index.html).
 
-var CACHE_VERSION = "bbs-v109";
+var CACHE_VERSION = "bbs-v110";
 
 var PRECACHE = [
     "./",
     "./index.html",
-    "./styles.css?v=90",
+    "./styles.css?v=91",
     "./data.js",
     "./validator.js",
     "./query.js",
@@ -21,7 +21,7 @@ var PRECACHE = [
     "./wardrobe-templates.js",
     "./cloth-data.js?v=3",
     "./heritage.js?v=2",
-    "./attract-shader.js?v=2",
+    "./attract-shader.js?v=3",
     "./weave-engine.js?v=3",
     "./garment-photo.js?v=18",
     "./fabric-visualiser.js?v=26",
